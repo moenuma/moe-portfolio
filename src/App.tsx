777ba0c933b0e4a03fda69c4e96134d47b62f89b
Moe N <moenumasawa@gmail.com>
2022-08-31 16:07:05 +0900

    Migrate Home to pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import styled from '@emotion/styled';
 import './App.css';
 import './star.scss';
 import { Sidebar } from './components/Sidebar';
+import { Home } from './pages/Home';
 
 export const App: React.FC = () => {
   return (
@@ -11,48 +12,13 @@ export const App: React.FC = () => {
       <div className='stars2'></div>
       <div className='stars3'></div>
       <Sidebar />
-      <Home id="home">
-        <Title>
-          <Heading>Moe</Heading>
-          <Emoji>👩🏻‍💻💬👩🏻‍🎨💡</Emoji><br/>
-          <Heading>Numasawa.</Heading>
-        </Title>
-      </Home>
+      <Home />
       <Profile id="profile">
         <div>hiii</div>
       </Profile>
     </div>
   );
 }
-
-const Home = styled.section`
-  font-family: 'Roboto', sans-serif;
-  font-style: normal;
-  height: 100vh;
-  z-index: 100;
-`;
-
-const Title = styled.div`
-  padding-top: 22vw;
-  padding-left: 5%;
-`;
-
-const Heading = styled.h1`
-  display: inline;
-  font-style: normal;
-  font-weight: 700;
-  font-size: 11vw;
-  line-height: 9vw;
-  color: white;
-  margin: 0;
-`;
-
-const Emoji = styled.span`
-  font-size: 6vw;
-  line-height: 3vw;
-  margin-left: 10vw;
-  padding-bottom: 10vw;
-`;
 
 const Profile = styled.section`
   font-family: 'Roboto', sans-serif;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+export const Home: React.FC = () => {
+    return (
+    <HomeSection id="home">
+        <Title>
+        <Heading>Moe</Heading>
+        <Emoji>👩🏻‍💻💬👩🏻‍🎨💡</Emoji><br/>
+        <Heading>Numasawa.</Heading>
+        </Title>
+    </HomeSection>
+    );
+}
+
+const HomeSection = styled.section`
+  font-family: 'Roboto', sans-serif;
+  font-style: normal;
+  height: 100vh;
+  z-index: 100;
+`;
+
+const Title = styled.div`
+  padding-top: 22vw;
+  padding-left: 5%;
+`;
+
+const Heading = styled.h1`
+  display: inline;
+  font-style: normal;
+  font-weight: 700;
+  font-size: 11vw;
+  line-height: 9vw;
+  color: white;
+  margin: 0;
+`;
+
+const Emoji = styled.span`
+  font-size: 6vw;
+  line-height: 3vw;
+  margin-left: 10vw;
+  padding-bottom: 10vw;
+`;
