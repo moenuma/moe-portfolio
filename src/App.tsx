8720c17ba3d834f2bee6f7c2f180bee8369870cb
Moe N <moenumasawa@gmail.com>
2022-09-04 21:35:58 +0900

    Add experience component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
-import styled from '@emotion/styled';
 import './App.css';
 import './star.scss';
 import { Sidebar } from './components/Sidebar';
 import { Home } from './pages/Home';
 import { Profile } from './pages/Profile';
+import { Experience } from './pages/Experience';
 
 export const App: React.FC = () => {
   return (
     <>
-      <div className='stars'></div>
-      <div className='stars2'></div>
-      <div className='stars3'></div>
+      <div className='stars' />
+      <div className='stars2' />
+      <div className='stars3' />
       <Sidebar />
       <Home />
       <Profile />
+      <Experience />
     </>
   );
 }
