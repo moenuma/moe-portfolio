4edd027d547c096c753cdc01661d2fa35260e246
Moe N <moenumasawa@gmail.com>
2022-09-04 23:15:10 +0900

    Migrate ProfileSkillList to Profile

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,7 +1,47 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import ProfileImage from '../assets/profile-image.jpg';
-import { ProfileSkillList } from '../components/ProfileSkillList';
+
+interface ProfileSkillListProps {
+  title: string;
+  skillList: string[];
+  color: string;
+}
+
+const ProfileSkillList: React.FC<ProfileSkillListProps> = ({ title, skillList, color }) => {
+  const Item = styled.div`
+      font-weight: 300;
+      font-size: 1.2rem;
+      color: white;
+      background: ${color};
+      border-radius: 10px;
+      padding: 0.5rem 0;
+      margin: 0.4rem 0;
+  `;
+  const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    width: 15rem;
+  `;
+  const Title = styled.div`
+    font-size: 1.2rem;
+    font-weight: 400;
+    margin-bottom: 0.5rem;
+    color: white;
+  `;
+  
+  return (
+  <Wrapper>
+      <Title>{title}</Title>
+      {skillList.map((obj) => {
+          return <Item>{obj}</Item>
+      })}
+  </Wrapper>
+  );
+}
+
+
 
 export const Profile: React.FC = () => {
   const backendSkills = ['Python (+ Django)', 'Java (+ Spring Boot)', 'C', 'SQL', 'MongoDB'];
