b96a592ad15e7543ef8d07d91984916712a5b8c8
Moe N <moenumasawa@gmail.com>
2022-09-07 01:39:43 +0900

    Add Swiper

--- src/pages/MyWork.tsx
+++ src/pages/MyWork.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import { Section, SectionTitle } from './Profile';
 import YogaImage from '../assets/yoga-image.jpg';
 import SocialMediaImage from '../assets/social-media-image.jpg';
 import HomeImage from '../assets/home-image.jpg';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 interface WorkComponentProps {
     title: string;
@@ -15,20 +20,19 @@ interface WorkComponentProps {
 const WorkComponent: React.FC<WorkComponentProps> = ({ title, description, languages, backgroundImage }) => {
     const Item = styled.div`
         display: flex;
-        margin: 0.5rem;
         border-radius: 10px;
         background-image: 
             linear-gradient(to bottom, rgba(255, 255, 255, 0), rgba(1, 1, 1, 0.35)), url(${backgroundImage});
-        background-size: 150% 100%;
+        background-size: 160% 100%;
         background-position: center;
         cursor: pointer;
-        aspect-ratio: 1/1.5;
+        aspect-ratio: 1/1.6;
         padding: 1rem;
         box-shadow: 0 0.75rem 2rem 0 rgba(0, 0, 0, 0.1);
         color: white;
     `;
     return (
-    <Item id="workItem">
+    <Item>
         <Content>
             <Title>{title}</Title>
             <Description>{description}</Description>
@@ -37,6 +41,7 @@ const WorkComponent: React.FC<WorkComponentProps> = ({ title, description, langu
             })}
         </Content>
     </Item>
+    
     );
 }
 
@@ -75,27 +80,55 @@ export const MyWork: React.FC = () => {
     );
     const workList = [socialDetails, alphaDetails, tartanDetails];
     return (
-    <Section id="myWork">
+    <MyWorkSection id="myWork">
         <SectionTitle>My Work</SectionTitle>
-        <Grid>
-            {workList.map((work) => {
-                return (
-                <WorkComponent
-                    title={work.title}
-                    description={work.description}
-                    languages={work.languages}
-                    backgroundImage={work.backgroundImage}
-                />)
-            })}
-        </Grid>
-    </Section>
+        <Flex>
+            <Swiper
+                modules={[Navigation, Pagination, Scrollbar, A11y]}
+                spaceBetween={25}
+                slidesPerView={3}
+                navigation={{
+                    nextEl: '.swiper-button-next',
+                    prevEl: '.swiper-button-prev',
+                }}
+                pagination={{ clickable: true }}
+                scrollbar={{ draggable: true }}
+            >
+                {workList.map((work) => {
+                    return (
+                    <SwiperSlide>
+                        <WorkComponent
+                            title={work.title}
+                            description={work.description}
+                            languages={work.languages}
+                            backgroundImage={work.backgroundImage}
+                        />
+                    </SwiperSlide>)
+                })}
+            </Swiper>
+            <div className="swiper-button-prev"></div>
+            <div className="swiper-button-next"></div> 
+        </Flex>
+    </MyWorkSection>
     );
 }
 
-const Grid = styled.div`
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    margin-top: 3rem;
+const Flex = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+const MyWorkSection = styled.section`
+  position: relative;
+  height: 100vh;
+`;
+
+const SectionTitle = styled.h2`
+    font-size: 3rem;
+    font-weight: 700;
+    margin: 0;
+    margin-left: 15.5rem;
+    margin-bottom: 2.5rem;
 `;
 
 const Title = styled.h3`
@@ -119,6 +152,7 @@ const Content = styled.div`
 const Language = styled.span`
     margin-right: 0.5rem;
     color: var(--light-gray-text-color);
+    display: inline-block;
     &:before {
         content: "#";
     }
