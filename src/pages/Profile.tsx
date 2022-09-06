37ff101f0c8f301eeaeda5716dcd91c55d9e96e0
Moe N <moenumasawa@gmail.com>
2022-09-06 14:36:54 +0900

    Minor change to UI

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -12,21 +12,29 @@ const ProfileSkillList: React.FC<ProfileSkillListProps> = ({ title, skillList, c
   const Item = styled.div`
       font-weight: 300;
       font-size: 1.2rem;
-      color: white;
-      background: ${color};
+      color: black;
       border-radius: 10px;
       padding: 0.5rem 0;
       margin: 0.4rem 0;
+      background: rgba(255, 255, 255, 0.5);
+      box-shadow: 0 0.5rem 2rem 0 rgba(0, 0, 0, 0.1);
+      border-radius: 1.5rem;
+      border: 1.5px solid hsl(${color}, 90%, 80%);
   `;
   const Wrapper = styled.div`
     display: flex;
     flex-direction: column;
     text-align: center;
     width: 15rem;
+    background-color: rgba(255, 255, 255, 0.375);
+    padding: 1rem;
+    border-radius: 1.5rem;
+    box-shadow: 0 0.75rem 2rem 0 hsl(${color}, 50%, 70%);
   `;
   const Title = styled.div`
-    font-size: 1.2rem;
-    font-weight: 400;
+    font-size: 1.25rem;
+    font-weight: 700;
+    font-style: italic;
     margin-bottom: 0.5rem;
     color: var(--primary-text-color);
   `;
@@ -51,15 +59,15 @@ export const Profile: React.FC = () => {
       <ImageTextsWrapper>
         <MyImage src={ProfileImage} alt="My photo"/>
         <Texts>
-          <Text>I am a fourth year student, majoring in computer science at the University of Alberta. </Text><br/>
-          <Text>I was born and raised in Japan. But I moved to Malaysia when I was 14 and spent 5 years living there. After high school, I decided to pursue my bachelor's degree in Canada, and here I am! </Text><br/>
+          <Text>I am a fourth year student, majoring in computer science at the <Bold>University of Alberta</Bold>. </Text><br/>
+          <Text>I was born and raised in <Bold>Japan</Bold>. But I moved to <Bold>Malaysia</Bold> when I was 14 and spent 5 years living there. After high school, I decided to pursue my bachelor's degree in <Bold>Canada</Bold>, and here I am! </Text><br/>
           <Text>My inquiring mind never stops me from learning more about software engineering every single day. I am a fast learner and have great adaptability as I have grown up in different parts of the world throughout my life.</Text><br/>
         </Texts>
       </ImageTextsWrapper>
       <SkillListsWrapper>
-        <ProfileSkillList title="Backend" skillList={backendSkills} color="#715FA6"/>
-        <ProfileSkillList title="Frontend" skillList={frontendSkills} color="#4C77B6"/>
-        <ProfileSkillList title="Others" skillList={otherSkills} color="#606060"/>
+        <ProfileSkillList title="Backend" skillList={backendSkills} color="var(--hue)"/>
+        <ProfileSkillList title="Frontend" skillList={frontendSkills} color="var(--hue-complimentary1)"/>
+        <ProfileSkillList title="Others" skillList={otherSkills} color="var(--hue-complimentary2)"/>
       </SkillListsWrapper>
     </ProfileSection>
     );
@@ -68,21 +76,21 @@ export const Profile: React.FC = () => {
 const ProfileSection = styled.section`
   height: 100vh;
   z-index: 100;
-  padding-left: 19rem;
+  padding-left: 15rem;
   padding-right: 20rem;
-  padding-top: 5rem;
+  padding-top: 3rem;
 `;
 
 const SectionTitle = styled.h2`
   font-size: 3rem;
-  font-weight: 550;
+  font-weight: 700;
   margin: 0;
-  text-align: center;
+  margin-left: 0.5rem;
 `;
 
 const Text = styled.div`
   display: inline-block;
-  font-size: 1.1rem;
+  font-size: 1.15rem;
   font-weight: 400;
   margin: 0;
   margin-top: 0.5rem;
@@ -91,16 +99,18 @@ const Text = styled.div`
 
 const Texts = styled.div`
   text-align: center;
-  margin-left: 3rem;
+  margin: 0;
+  margin-left: 1rem;
+  padding: 0rem 2rem;
 `;
 
 const MyImage = styled.img`
-  width: 200px;
-  // border-radius: 15px;
+  width: 205px;
+  border-radius: 1.5rem;
 `;
 
 const ImageTextsWrapper = styled.div`
-  margin-top: 2.5rem;
+  margin-top: 1.5rem;
   display: flex;
   align-items: center;
 `;
@@ -108,5 +118,9 @@ const ImageTextsWrapper = styled.div`
 const SkillListsWrapper = styled.div`
   display: flex;
   justify-content: space-between;
-  margin-top: 5rem;
+  margin-top: 4rem;
+`;
+
+const Bold = styled.span`
+  font-weight: 600;
 `;
